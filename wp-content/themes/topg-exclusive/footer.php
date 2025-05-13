04e355da9a6aab272a1ee75fe189a7c61cd4b489
shehabah16 <shehabah16@gmail.com>
2025-05-13 17:28:04 +0300

    Add TIXAE chat widget

---
+++ wp-content/themes/topg-exclusive/footer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The template for displaying the footer
+ */
+
+?>
+    </div><!-- #content -->
+
+    <footer id="colophon" class="site-footer">
+        <!-- Your existing footer content -->
+    </footer>
+
+    <?php get_template_part('chat-widget'); ?>
+    <?php wp_footer(); ?>
+
+</body>
+</html>
